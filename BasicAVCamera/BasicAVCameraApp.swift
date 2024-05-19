fd5fa5653ec36d663c7c298d9de8f9d0494c27e5
itsuki <itsuki.enjyoy@gmail.com>
2024-05-19 09:41:26 +0900

    Initial Commit

---
+++ BasicAVCamera/BasicAVCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicAVCameraApp.swift
+//  BasicAVCamera
+//
+//  Created by Itsuki on 2024/05/19.
+//
+
+import SwiftUI
+
+@main
+struct BasicAVCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
